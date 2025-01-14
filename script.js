ade265b4e7f0332b2701e4bd041f218ef4aac985
Khandoke Shahil Rahman <167141448+Shahil-Rahman@users.noreply.github.com>
2025-01-15 04:10:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const btnMenu = document.querySelector('.trigger'),
+      menuclass = document.querySelector('nav');
+btnMenu.addEventListener('click', function(){
+    menuclass.classList.toggle('showmenu')
+})
+
+//on off light 
+const btnLight = document.querySelector('.t-light'),
+      addclass = document.querySelector('.site');
+btnLight.addEventListener('click', function() {
+    addclass.classList.toggle('turnon')
+})
+
+//carousel
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+        641: {
+            slidesPerView: 2,
+        },
+
+        992: {
+            slidesPerView: 3,
+        },
+    },
+  });
